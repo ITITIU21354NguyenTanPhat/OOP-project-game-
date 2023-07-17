8684e7362255ba0cddbc8c8f26db0b4f898d3bfd
Nguyen Tan Phat (ITITIU21354) <125816527+ITITIU21354NguyenTanPhat@users.noreply.github.com>
2023-07-17 16:25:02 +0700

    Add files via upload

---
+++ KeyBoard.java
@@ -0,0 +1,17 @@
+package KeyBoard;
+
+// import java.awt.event.KeyEvent;
+// import java.awt.event.KeyListener;
+
+
+
+public interface KeyBoard{
+
+
+
+    public void keyPressed(int key_code);
+
+
+    public void keyReleased(int key_code);
+    
+}
